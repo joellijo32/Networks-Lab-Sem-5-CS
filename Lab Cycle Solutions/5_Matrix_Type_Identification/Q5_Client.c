210007fa381ddad4d18e3ce8c8dcee568731c342
Joel Lijo Mathew <joellijo4@gmail.com>
2026-08-05 18:27:16 +0530

    Q5 client, server code added

--- Lab Cycle Solutions/5_Matrix_Type_Identification/Q5_Client.c
+++ Lab Cycle Solutions/5_Matrix_Type_Identification/Q5_Client.c
@@ -1,23 +1,120 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <time.h>
 
-int main(){
-	int socketFD = socket(AF_INET, SOCK_STREAM, 0);
+void fillLower(int n, int matrix[][n]);
+void fillDiagonal(int n, int matrix[][n]);
+void fillUpper(int n, int matrix[][n]);
+void fillRandom(int n, int matrix[][n]);
+void display(int n, int matrix[][n]);
 
-	char* ip = "192.178.24.142";
-	struct sockaddr_in address ;
-	address.sin_family = AF_INET;
-	address.sin_port = 2000;
-	inet_pton(AF_INET, &address, &address.sin_addr.s_addr);
-
-	int status = connect(socketFD, &address, sizeof(address));
-
-	if(status == 0) printf("\nConnection was successfull...\n");
-
-	return 0;
+void fillUpper(int n, int matrix[][n]) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (j >= i)
+                matrix[i][j] = rand() % 50 + 1;
+            else
+                matrix[i][j] = 0;
+        }
+    }
 }
 
+void fillLower(int n, int matrix[][n]) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (j <= i)
+                matrix[i][j] = rand() % 50 + 1;
+            else
+                matrix[i][j] = 0;
+        }
+    }
+}
+
+void fillDiagonal(int n, int matrix[][n]) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i == j)
+                matrix[i][j] = rand() % 50 + 1;
+            else
+                matrix[i][j] = 0;
+        }
+    }
+}
+
+void fillRandom(int n, int matrix[][n]) {
+    int choose = rand() % 3;
+
+    if (choose == 0)
+        fillUpper(n, matrix);
+    else if (choose == 1)
+        fillLower(n, matrix);
+    else
+        fillDiagonal(n, matrix);
+}
+
+void display(int n, int matrix[][n]) {
+    printf("\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%3d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+
+    int sockFD = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sockFD < 0) {
+        perror("Socket creation failed");
+        return 1;
+    }
+
+    struct sockaddr_in address;
+
+    address.sin_family = AF_INET;
+    address.sin_port = htons(2095);
+    address.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    int status = connect(sockFD, (struct sockaddr *)&address, sizeof(address));
+
+    if (status < 0) {
+        perror("Connection failed");
+        close(sockFD);
+        return 1;
+    }
+
+    printf("Connection was successful...\n");
+
+    char buffer[1024] = "Hello from client";
+
+    send(sockFD, buffer, strlen(buffer), 0);
+
+    memset(buffer, 0, sizeof(buffer));
+
+    read(sockFD, buffer, sizeof(buffer));
+
+    printf("Message from server: %s\n", buffer);
+
+    memset(buffer, 0, sizeof(buffer));
+
+    srand(time(NULL));
+    printf("Enter N = "); int n;
+    scanf("%d", &n);
+    int matrix[n][n];
+    fillRandom(n, matrix);
+	printf("\nMatrix Sent from client: \n");
+	display(n, matrix);
+
+	send(sockFD, &n, sizeof(n), 0);
+	send(sockFD, matrix, sizeof(matrix), 0);
+    close(sockFD);
+    return 0;
+}
---
+++ Lab Cycle Solutions/5_Matrix_Type_Identification/Q5_Server.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+int checkMatrix(int n, int matrix[][n]){
+	int isUpper = 1, isLower = 1, isDiagonal = 1;
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < n;j++){
+			if(i < j && matrix[i][j] != 0) isLower = 0;
+			if(i > j && matrix[i][j] != 0) isUpper = 0;
+			if(i != j && matrix[i][j] != 0) isDiagonal = 0;
+		}
+	}
+	if(isDiagonal) return 2;
+	else if(isUpper) return 0;
+	else if(isLower) return 1;
+}
+
+int main() {
+    int sockFD = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sockFD < 0) {
+        perror("Socket creation failed");
+        return 1;
+    }
+
+    struct sockaddr_in address, client;
+    socklen_t len = sizeof(client);
+
+    char buffer[1024];
+//    char* ip = "192.168.1.5";
+    address.sin_family = AF_INET;
+    address.sin_port = htons(2095);
+    address.sin_addr.s_addr = INADDR_ANY;
+//	inet_pton(AF_INET, ip, &address.sin_addr.s_addr);
+
+    if (bind(sockFD, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("Bind failed");
+        close(sockFD);
+        return 1;
+    }
+
+    listen(sockFD, 5);
+
+    printf("Waiting for client...\n");
+
+    int newsock = accept(sockFD, (struct sockaddr *)&client, &len);
+
+    if (newsock < 0) {
+        perror("Accept failed");
+        close(sockFD);
+        return 1;
+    }
+
+    memset(buffer, 0, sizeof(buffer));
+
+    read(newsock, buffer, sizeof(buffer));
+
+    printf("Message: %s\n", buffer);
+
+    send(newsock, "Hello from server", 17, 0);
+
+	int n;
+	read(newsock, &n, sizeof(n));
+	int matrix[n][n];
+	read(newsock, &matrix, sizeof(matrix));
+
+	int result = checkMatrix(n, matrix);
+	printf("The matrix recieved is: ");
+	if(result == 0) printf("Upper Triangle\n");
+	else if(result == 1) printf("Lower Triangle\n");
+	else printf("Diagonal Matrix\n");
+
+
+    close(newsock);
+    close(sockFD);
+
+    return 0;
+}
