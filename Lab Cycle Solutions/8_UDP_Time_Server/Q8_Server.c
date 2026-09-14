544c7f17cc813cd0f4600a578f9cb03c6b390d32
Joel Lijo Mathew <joellijo4@gmail.com>
2026-09-14 14:28:36 +0530

    Q8 server testing code

--- Lab Cycle Solutions/8_UDP_Time_Server/Q8_Server.c
+++ Lab Cycle Solutions/8_UDP_Time_Server/Q8_Server.c
@@ -7,7 +7,7 @@
 #include <netinet/in.h>
 #include <ctype.h>
 #include <unistd.h>
-
+#include <time.h>
 
 #define BUFFER_SIZE 1024
 int port;
@@ -15,10 +15,11 @@ int port;
 int main (void) {
 	char buffer[BUFFER_SIZE];
 	struct sockaddr_in server_addr, client_addr;
+	socklen_t client_len = sizeof(client_addr);
 
 	printf("Enter port: "); scanf("%d", &port);
 
-	int sock_fd = socket (AF_INET, (struct sockaddr*)&server_addr, sizeof(server_addr));
+	int sock_fd = socket(AF_INET, SOCK_DGRAM, 0);
 
 	if (sock_fd < 0) {
 		perror("Socket creation failed");
@@ -34,4 +35,24 @@ int main (void) {
 		return 1;
 	}
 
-	
+	printf ("\nWaiting for client...\n");
+
+	memset (buffer, 0, sizeof(buffer));
+
+	recvfrom(sock_fd, buffer, BUFFER_SIZE, 0, (struct sockaddr*)&client_addr, &client_len);
+
+	printf("\nMessage from client: %s\n", buffer);
+
+	memset(buffer, 0, sizeof(buffer));
+
+	sendto(sock_fd, "Hello from Server", 17, 0, (struct sockaddr*)&client_addr, client_len);
+
+	memset(buffer, 0, sizeof(buffer));
+
+	recvfron(sock_fd, buffer, BUFFER_SIZE, 0, (struct sockaddr*)&client_addr, &client_len);
+
+	printf("%s from client\n", buffer);
+
+	close(sock_fd);
+	return 0;
+}
